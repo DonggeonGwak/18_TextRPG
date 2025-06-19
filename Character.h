d07c82255aebc0a27ba9bb438c41acd258e5cebd
Eldia <nsw22122@gmail.com>
2025-06-19 12:55:32 +0900

    Create Character.h

---
+++ Character.h
@@ -0,0 +1,118 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Item.h"
+
+class Character
+{
+private:
+	std::string _name;
+	int _hp;
+	int _Max_Heart;
+	int _level;
+	int _attack;
+	int _gold;
+	int _Experience_Point;
+	int _Max_Experience_Point;
+	vector<Item*> _inventory;
+
+public:
+
+	Character(const std::string& name_in)
+	{
+		_name					= name_in;
+		_hp						= 200;
+		_Max_Heart				= 200;
+		_level					= 1;
+		_attack					= 30;
+		_Experience_Point		= 0;
+		_Max_Experience_Point	= 100;
+		_gold					= 0;
+	}
+
+	void setHp(int value)
+	{
+		if (_hp > 0)
+			_hp = value;
+		else if (value <= 0)
+			_hp = 0;
+		else if(value >= _Max_Heart)
+			_hp = _Max_Heart;
+	}
+
+	void setAttack(int attack)
+	{
+		_attack = attack;
+	}
+
+	int getHp()
+	{
+		return _hp;
+	}
+
+	int getAttack()
+	{
+		return _attack;
+	}
+
+	int getLevel()
+	{
+		return _level;
+	}
+
+	//스테이터스 확인하는 함수
+	void displayStatus()
+	{
+		cout << "\n캐릭터의 스테터스" << endl;
+		cout << "HP: " << _hp << endl;
+		cout << "Level: " << _level << endl;
+		cout << "공격력: " << _attack << endl;
+		cout << "소지금: " << _gold << endl;
+		cout << "경험치: " << _Experience_Point << endl;
+	}
+
+	//레벨 업 하는 함수
+	void LevelUp()
+	{
+		if (_level < 10)
+		{
+			_level += 1;
+			_hp += (_level * 20);
+			_Max_Heart += (_level * 20);
+			_attack += (_level * 5);
+			cout << "level이 올랐습니다! 현재 level: " << _level << endl;
+		}
+		else
+		{
+			cout << "최대레벨입니다! 더 이상 level Up이 불가능 합니다." << endl;
+		}
+	}
+
+	void addItem()
+	{
+
+	}
+
+	//가지고 있는 아이템을 보는 함수.
+	void ItemList(int index)
+	{
+		if (_inventory.empty())
+		{
+			cout << "\n현재 가지고 있는 아이템이 없습니다." << endl;
+		}
+		else
+		{
+			cout << "\n인벤토리에 있는 아이템" << endl;
+
+			for (size_t i = 0; i < _inventory.size(); i++)
+			{
+				cout << _inventory[i] << endl;
+			}
+			cout << endl;
+		}
+	}
+
+
+};
